2a63688bd31620b1e5e433bc2d233ee31be7d820
M. Anthony <mikeeanthony@gmail.com>
2021-07-07 17:06:10 -0500

    commit updates to testimonials

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -5,7 +5,7 @@ import { testimonials } from "../data";
 export default function Testimonials() {
   return (
     <section id="testimonials">
-      <div className="container px-5 py-10 mx-auto text-center">
+      <div className="container px-5 py-10 mx-auto text-left">
         <UsersIcon className="w-10 inline-block mb-4" />
         <h1 className="sm:text-4xl text-3xl font-medium title-font text-white mb-12">
           Testimonials
