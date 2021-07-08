7b3900b09a0405728ee9a6bc576f78c70baab8d6
M. Anthony <mikeeanthony@gmail.com>
2021-07-07 21:19:38 -0500

    update svg icon on testimonials page

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { TerminalIcon, UsersIcon } from "@heroicons/react/solid";
+import { ThumbUpIcon, UsersIcon } from "@heroicons/react/solid";
 import { testimonials } from "../data";
 
 export default function Testimonials() {
@@ -14,7 +14,7 @@ export default function Testimonials() {
           {testimonials.map((testimonial) => (
             <div className="p-4 md:w-1/2 w-full">
               <div className="h-full bg-gray-800 bg-opacity-40 p-8 rounded">
-                <TerminalIcon className="block w-8 text-gray-500 mb-4" />
+                <ThumbUpIcon className="block w-8 text-gray-500 mb-4" />
                 <p className="leading-relaxed mb-6">{testimonial.quote}</p>
                 <div className="inline-flex items-center">
                   <img
