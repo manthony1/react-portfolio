f242d3f922b184d87688c82e88da22703ef30a73
M. Anthony <mikeeanthony@gmail.com>
2021-07-08 21:16:55 -0500

    local img files

--- src/data.js
+++ src/data.js
@@ -1,4 +1,7 @@
-export const projects = [
+export const projects = [];
+
+
+/*export const projects = [
   {
     title: "React Reserve",
     subtitle: "MERN Stack",
@@ -32,33 +35,34 @@ export const projects = [
     link: "https://reactbootcamp.com",
   },
 ];
+*/
 
 export const testimonials = [
   {
     quote:
       "I hired Mike to lead the Marketing Department at Sphere and in just a few short weeks I could feel the impact of his work. Mike understood the goals, developed actionable plans and executed quickly and expertly. Mike’s Digital Marketing expertise spans across MarTech, content development, inbound/outbound demand generation and digital advertising among other things. Since Mike started at Sphere, he has grown professionally by taking on more responsibility and adding members to his growing team. He is truly a well rounded individual, very smart and easy to work with, and has shown excellent ability to lead the digital marketing team.",
-    image: "https://media-exp3.licdn.com/dms/image/C4E03AQH5ndHcMjoHBA/profile-displayphoto-shrink_100_100/0/1583593167068?e=1631145600&v=beta&t=nDJY_hVoFEfD5BsjowAtnguLf-YaPddwXjT8_skAGzc",
+    image: "./il.jpg",
     name: "Isabel Londoño",
     company: "Sphere Partners",
   },
   {
     quote:
       "I really enjoyed working with Mike at Saba Software. During his time, he showed me that his strength was leading technical digital marketing projects that needed to be rolled out to global teams. Mike is a 'go-getter' and a great team mate.",
-    image: "https://media-exp3.licdn.com/dms/image/C4D03AQH39HyaNdAbbg/profile-displayphoto-shrink_100_100/0/1592873179081?e=1631145600&v=beta&t=nYxYo_PFnzlZ3C6ps6IbOKXgDSMDBMfj4Fci7pxitAA",
+    image: "./pdr.jpg",
     name: "Peter de Rosa",
     company: "Saba/Cornerstone on Demand",
   },
   {
     quote:
       "Mike is a top-notch digital marketing professional who I managed and worked with over the last few years. During that time Mike was instrumental in our efforts to build a productive digital marketing lead generation program that dramatically impacted our sales. Mike was also our go-to expert for building, integrating and analyzing our marketing reporting. Mike's attention to detail and the ability to grasp and integrate new and different MarTech have been a huge asset to our team. I'm confident that Mike can handle most any digital marketing challenge presented.",
-    image: "https://media-exp3.licdn.com/dms/image/C4E03AQH15KHhN5bhUQ/profile-displayphoto-shrink_100_100/0/1546136169839?e=1631145600&v=beta&t=A7Bu3VIHZ2Bg7IMuR0qHE9c4Cho6K1CgEGfK-j5n8QY",
+    image: "./gl.jpg",
     name: "Greg Latson",
     company: "Saba Software/Cornerstone on Demand",
   },
   {
     quote:
       "Mike is a rock star! He is really focused on aligning sale objectives with Marketing. Has a passion for ABM and a wiz at powerpoint! Mike really understands what needs to be done to achieve goals and will focus on results. I really enjoy working with him.",
-    image: "https://media-exp3.licdn.com/dms/image/C4E03AQEZdvigA0NKJQ/profile-displayphoto-shrink_100_100/0/1576601936057?e=1631145600&v=beta&t=4eY2BUCPNHUXTFITYfv7AITUsPMVC0ic6dDCEZzaASE",
+    image: "./lf.jpg",
     name: "Laura Fuller",
     company: "Lumesse, Saba Software",
   },
