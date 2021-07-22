d4b701d351446cd14ac2e199710fcd513992e446
M. Anthony <mikeeanthony@gmail.com>
2021-07-21 21:15:30 -0500

    update contact to fix form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -63,7 +63,8 @@ export default function Contact() {
           </div>
         </div>
         <form
-          netlify
+          data-netlify="true"
+          netlify-honeypot="bot-field" hidden
           name="contact"
           onSubmit={handleSubmit}
           className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
