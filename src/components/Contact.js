d909106ea9adea5a3f9203960e0bf67d5ca6117f
M. Anthony <mikeeanthony@gmail.com>
2021-07-22 13:38:06 -0500

    added hidden input to form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -68,6 +68,7 @@ export default function Contact() {
           name="contact"
           onSubmit={handleSubmit}
           className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
+          <input type="hidden" name="form-name" value="contact" />
           <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
             Hire Me
           </h2>
