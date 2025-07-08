56638a43b5c16c0907fc8a622245d675dc1b6847
Mike Anthony <mikeeanthony@gmail.com>
2025-07-08 17:02:03 -0500

    "Update About text"

--- src/components/About.js
+++ src/components/About.js
@@ -6,11 +6,13 @@ export default function About() {
       <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
-            Hi, I'm Mike.<br />
-            <br className="hidden lg:inline-block" />Technical PM, Process Improvement (Workflow) Analyst.<br /> Marketing Operations and Data Analyst.<br />Code Proficient.
-
+            <br className="hidden lg:inline-block" />Hi, I'm Mike.<br />
+            <br className="hidden lg:inline-block" />Technical Project Manager<br />
+            <br className="hidden lg:inline-block" />Process Improvement (Workflow) Analyst.<br />
+            <br className="hidden lg:inline-block" />Code & AI Proficient.<br />
           </h1>
           <p className="mb-8 leading-relaxed text-green-600 text-lg">
+          Project Management Professsional (PMP)<br />
           ServiceNow Certified System Administrator (CSA)<br />
           ServiceNow Certified Implementation Specialist - IT Service Management (CIS - ITSM) - <i>In progress</i>
           </p>
